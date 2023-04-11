0e0b1b35281eb7aa44382a23897f5d3f57e2ac98
smartcoding51 <keinakano415@gmail.com>
2023-04-12 03:43:41 +0900

    update utils adding arbitrum goerli

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,10 +2,12 @@ import { SUPPORT_CHAIN_IDS } from "./enums";
 
 export const DECIMAL: { [chainId: number]: number } = {
   [SUPPORT_CHAIN_IDS.GOERLI]: 6,
-  [SUPPORT_CHAIN_IDS.MOONBEAM_ALPHA]: 18
+  [SUPPORT_CHAIN_IDS.MOONBEAM_ALPHA]: 18,
+  [SUPPORT_CHAIN_IDS.ARBITRUM_GOERLI]: 6
 };
 
 export const EXPLORER: { [chainId: number]: string } = {
   [SUPPORT_CHAIN_IDS.GOERLI]: "https://goerli.etherscan.io",
-  [SUPPORT_CHAIN_IDS.MOONBEAM_ALPHA]: "https://moonbase.moonscan.io"
+  [SUPPORT_CHAIN_IDS.MOONBEAM_ALPHA]: "https://moonbase.moonscan.io",
+  [SUPPORT_CHAIN_IDS.ARBITRUM_GOERLI]: "https://goerli.arbiscan.io"
 };
