d578025fb8f620f5143501eedc2e7eb3a0e9ad63
smartcoding51 <keinakano415@gmail.com>
2023-05-19 01:34:42 +0900

    remove 'Superhedge Products' title and unnecesary div

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,7 @@ import ProductList from "../components/products/List";
 const Home = () => {
   return (
     <div className={"py-12"}>
-      <span className={"bg-clip-text text-transparent bg-primary-gradient text-[68px] leading-[76px]"}>SuperHedge Products</span>
-      <div className='mt-7'>
-        <ProductList />
-      </div>
+      <ProductList />
     </div>
   );
 };
