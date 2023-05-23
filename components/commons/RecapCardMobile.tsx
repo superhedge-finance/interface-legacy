de91d4fc36808da78b1eb6902833a3ebe7218dc4
smartcoding51 <keinakano415@gmail.com>
2023-05-24 01:23:51 +0900

    add common component for RecapCardMobile

---
+++ components/commons/RecapCardMobile.tsx
@@ -0,0 +1,8 @@
+export const RecapCardMobile = ({ label, value, className }: { label: string; value: React.ReactNode; className?: string }) => {
+    return (
+      <div className={`flex md:flex-col flex-1 items-center justify-between h-[40px] md:h-[66px] bg-[#0000000a] rounded-[7px] p-3 ${className ?? ""}`}>
+        <p className='text-[16px] md:text-[12px] font-light text-gray-700'>{label}</p>
+        <h3 className='text-[16px] md:text-[16px] font-light text-black'>{value}</h3>
+      </div>
+    );
+};
